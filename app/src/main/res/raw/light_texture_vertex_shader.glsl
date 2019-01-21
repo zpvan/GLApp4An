d8990d17ba05c0506d90a01714f3aee34b1d9da6
zpvan <843762968@qq.com>
2019-01-21 15:27:13 +0800

    光照贴图-prepare

---
+++ app/src/main/res/raw/light_texture_vertex_shader.glsl
@@ -0,0 +1,23 @@
+
+uniform mat4 u_Model;
+uniform mat4 u_View;
+uniform mat4 u_Projection;
+uniform mat4 u_NormalizeModel;
+
+attribute vec4 a_Position;
+attribute vec3 a_Normalize;
+attribute vec2 a_TextureCoords;
+
+varying vec3 v_Normalize;
+varying vec3 v_FragPos;
+varying vec2 v_TextureCoords;
+
+void main()
+{
+    gl_Position = u_Projection * u_View * u_Model * a_Position;
+    v_FragPos = vec3(u_Model * a_Position);
+    v_Normalize = a_Normalize;
+    // v_Normalize = vec3(u_Model * vec4(a_Normalize, 1.0));
+    // v_Normalize = vec3(u_NormalizeModel * vec4(a_Normalize, 1.0));
+    // v_Normalize = mat3(transpose(inverse(u_Model)) * a_Normalize);
+}
