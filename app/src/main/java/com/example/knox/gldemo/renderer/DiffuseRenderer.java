7f69bd93828ce1396bf3b51f2caf217cb20397fb
zpvan <843762968@qq.com>
2019-01-21 11:11:18 +0800

    漫反射, 但是有bug, 即使旋转起来, 迎光面一直不会变

--- app/src/main/java/com/example/knox/gldemo/renderer/DiffuseRenderer.java
+++ app/src/main/java/com/example/knox/gldemo/renderer/DiffuseRenderer.java
@@ -2,7 +2,6 @@ package com.example.knox.gldemo.renderer;
 
 import android.content.Context;
 import android.opengl.GLES20;
-import android.opengl.GLES30;
 import android.opengl.GLSurfaceView;
 import android.opengl.Matrix;
 import android.util.Log;
@@ -25,22 +24,33 @@ public class DiffuseRenderer implements GLSurfaceView.Renderer {
 
     private static final String TAG = "DiffuseRenderer";
 
-    private static final int BYTES_OF_FLOAT = 4;
     private static final String U_MATRIX = "u_Matrix";
+    private static final String U_MODEL = "u_Model";
+    private static final String U_VIEW = "u_View";
+    private static final String U_PROJECTION = "u_Projection";
     private static final String A_POSITION = "a_Position";
     private static final String U_OBJECTCOLOR = "u_objectColor";
     private static final String U_LIGHTCOLOR = "u_lightColor";
+    private static final String A_NORMALIZE = "a_Normalize";
+    private static final String U_LIGHTPOS = "u_lightPos";
+    private static final String U_NORMALIZEMODEL = "u_NormalizeModel";
     private static final int POSITION_COMPONENT_COUNT = 3;
+    private static final int NORMALIZE_COMPONENT_COUNT = 3;
 
     private Context mCtx;
     private int mObjectProgram;
     private int mLightProgram;
-    private int uObjectMatrix;
+    private int uObjectModel;
+    private int uObjectView;
+    private int uObjectProjection;
     private int aObjectPosition;
+    private int aNormalize;
     private int uObjectObjecColor;
     private int uObjectLightColor;
     private int uLightMatrix;
     private int aLightPosition;
+    private int uLightPos;
+    private int uNormalizeModel;
 
     float boxVertices[] = {
             //  X      Y      Z
@@ -141,6 +151,8 @@ public class DiffuseRenderer implements GLSurfaceView.Renderer {
     private float[] mViewProjectionMatrix = new float[16];
     private float[] mModelMatrix = new float[16];
     private float[] mMVPMatrix = new float[16];
+    private float[] mInvertModelMatrix = new float[16];
+    private float[] mTransposeInvertModelMatrix = new float[16];
 
     public DiffuseRenderer(Context context) {
         mCtx = context;
@@ -148,40 +160,45 @@ public class DiffuseRenderer implements GLSurfaceView.Renderer {
 
     @Override
     public void onSurfaceCreated(GL10 gl, EGLConfig config) {
-        GLES30.glClearColor(0f, 0f, 0f, 1f);
+        GLES20.glClearColor(0f, 0f, 0f, 1f);
 
         mObjectProgram = EsUtil.shaderCode2Program(
-                ResUtil.readResource2String(mCtx, R.raw.ambient_vertex_shader),
-                ResUtil.readResource2String(mCtx, R.raw.ambient_fragment_shader));
+                ResUtil.readResource2String(mCtx, R.raw.diffuse_vertex_shader),
+                ResUtil.readResource2String(mCtx, R.raw.diffuse_fragment_shader));
 
         if (mObjectProgram == 0) {
             Log.e(TAG, "onSurfaceCreated: program failed!");
             return;
         }
 
-        GLES30.glUseProgram(mObjectProgram);
+        GLES20.glUseProgram(mObjectProgram);
 
-        uObjectMatrix = GLES20.glGetUniformLocation(mObjectProgram, U_MATRIX);
+        uObjectModel = GLES20.glGetUniformLocation(mObjectProgram, U_MODEL);
+        uObjectView = GLES20.glGetUniformLocation(mObjectProgram, U_VIEW);
+        uObjectProjection = GLES20.glGetUniformLocation(mObjectProgram, U_PROJECTION);
         aObjectPosition = GLES20.glGetAttribLocation(mObjectProgram, A_POSITION);
         uObjectObjecColor = GLES20.glGetUniformLocation(mObjectProgram, U_OBJECTCOLOR);
         uObjectLightColor = GLES20.glGetUniformLocation(mObjectProgram, U_LIGHTCOLOR);
+        aNormalize = GLES20.glGetAttribLocation(mObjectProgram, A_NORMALIZE);
+        uLightPos = GLES20.glGetAttribLocation(mObjectProgram, U_LIGHTPOS);
+        uNormalizeModel = GLES20.glGetAttribLocation(mObjectProgram, U_NORMALIZEMODEL);
 
         GLES20.glEnable(GLES20.GL_DEPTH_TEST);
 
-        GLES30.glUseProgram(0);
+        GLES20.glUseProgram(0);
 
         mLightProgram = EsUtil.shaderCode2Program(
-                ResUtil.readResource2String(mCtx, R.raw.phonglight_vertex_shader),
+                ResUtil.readResource2String(mCtx, R.raw.light_vertex_shader),
                 ResUtil.readResource2String(mCtx, R.raw.light_fragment_shader));
 
-        GLES30.glUseProgram(mLightProgram);
+        GLES20.glUseProgram(mLightProgram);
 
         uLightMatrix = GLES20.glGetUniformLocation(mLightProgram, U_MATRIX);
         aLightPosition = GLES20.glGetAttribLocation(mLightProgram, A_POSITION);
 
         GLES20.glEnable(GLES20.GL_DEPTH_TEST);
 
-        GLES30.glUseProgram(0);
+        GLES20.glUseProgram(0);
     }
 
     @Override
@@ -199,41 +216,59 @@ public class DiffuseRenderer implements GLSurfaceView.Renderer {
         Matrix.multiplyMM(mViewProjectionMatrix, 0, mProjectionMatrix, 0, mViewMatrix, 0);
 
         mBaseTimeMs = System.currentTimeMillis();
+
+        GLES20.glUseProgram(mObjectProgram);
+
+        GLES20.glUniformMatrix4fv(uObjectProjection, 1, false, mProjectionMatrix, 0);
+        GLES20.glUniformMatrix4fv(uObjectView, 1, false, mViewMatrix, 0);
+
+        GLES20.glUseProgram(0);
     }
 
     @Override
     public void onDrawFrame(GL10 gl) {
         GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT | GLES20.GL_DEPTH_BUFFER_BIT);
 
-        GLES30.glUseProgram(mObjectProgram);
+        GLES20.glUseProgram(mObjectProgram);
         // 画立方体
         EsUtil.VertexAttribArrayAndEnable(aObjectPosition, POSITION_COMPONENT_COUNT, GLES20.GL_FLOAT,
                 false, 0, boxVertices);
 
+        EsUtil.VertexAttribArrayAndEnable(aNormalize, NORMALIZE_COMPONENT_COUNT, GLES20.GL_FLOAT,
+                false, 0, boxNormalizedVertices);
+
         Matrix.setIdentityM(mModelMatrix, 0);
-        Matrix.translateM(mModelMatrix, 0, 0, 0, -2.0f);
-        Matrix.rotateM(mModelMatrix, 0, 2f * ((System.currentTimeMillis() - mBaseTimeMs) / 100), 0.5f, 1f, 0f);
-        Matrix.multiplyMM(mMVPMatrix, 0, mViewProjectionMatrix, 0, mModelMatrix, 0);
-        GLES20.glUniformMatrix4fv(uObjectMatrix, 1, false, mMVPMatrix, 0);
+        Matrix.translateM(mModelMatrix, 0, 0, 0, -10.0f);
+        // Matrix.rotateM(mModelMatrix, 0, 2f * ((System.currentTimeMillis() - mBaseTimeMs) / 100), 0.5f, 1f, 0f);
+        Matrix.rotateM(mModelMatrix, 0, 50f, 0.5f, 1f, 0f);
+        GLES20.glUniformMatrix4fv(uObjectModel, 1, false, mModelMatrix, 0);
+
+        Matrix.invertM(mInvertModelMatrix, 0, mModelMatrix, 0);
+        // Log.e(TAG, "onDrawFrame: [modelMatrix]=[" + MatrixUtil.print(mModelMatrix) + "]");
+        // Log.e(TAG, "onDrawFrame: [invertModelMatrix]=[" + MatrixUtil.print(mInvertModelMatrix) + "]");
+        Matrix.transposeM(mTransposeInvertModelMatrix, 0, mInvertModelMatrix, 0);
+        // Log.e(TAG, "onDrawFrame: [transposeInvertModelMatrix]=[" + MatrixUtil.print(mTransposeInvertModelMatrix) + "]");
+        GLES20.glUniformMatrix4fv(uNormalizeModel, 1, false, mTransposeInvertModelMatrix, 0);
 
         GLES20.glUniform3f(uObjectObjecColor, 1.0f, 0.5f, 0.31f);
         GLES20.glUniform3f(uObjectLightColor, 1.0f, 1.0f, 1.0f);
+        GLES20.glUniform3f(uLightPos, 10.0f, 0, -10.0f);
 
         GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0,
                 boxVertices.length / POSITION_COMPONENT_COUNT);
-        GLES30.glUseProgram(0);
+        GLES20.glUseProgram(0);
 
-        GLES30.glUseProgram(mLightProgram);
+        GLES20.glUseProgram(mLightProgram);
         // 画光源
         EsUtil.VertexAttribArrayAndEnable(aLightPosition, POSITION_COMPONENT_COUNT, GLES20.GL_FLOAT,
                 false, 0, boxVertices);
         Matrix.setIdentityM(mModelMatrix, 0);
-        Matrix.translateM(mModelMatrix, 0, 10.0f, 0, -20.0f);
+        Matrix.translateM(mModelMatrix, 0, 10.0f, 0, -10.0f);
         Matrix.multiplyMM(mMVPMatrix, 0, mViewProjectionMatrix, 0, mModelMatrix, 0);
         GLES20.glUniformMatrix4fv(uLightMatrix, 1, false, mMVPMatrix, 0);
 
         GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0,
                 boxVertices.length / POSITION_COMPONENT_COUNT);
-        GLES30.glUseProgram(0);
+        GLES20.glUseProgram(0);
     }
 }
--- app/src/main/res/raw/diffuse_fragment_shader.glsl
+++ app/src/main/res/raw/diffuse_fragment_shader.glsl
@@ -3,12 +3,21 @@ precision mediump float;
 uniform vec3 u_objectColor;
 uniform vec3 u_lightColor;
 
+uniform vec3 u_lightPos;
+
 varying vec3 v_Normalize;
+varying vec3 v_FragPos;
 
 void main()
 {
-     float ambientStrength = 0.2;
-     vec3 ambient = ambientStrength * u_lightColor;
-     vec3 result = ambient * u_objectColor;
-     gl_FragColor = vec4(result, 1.0);
+    float ambientStrength = 0.2;
+    vec3 ambient = ambientStrength * u_lightColor;
+
+    vec3 norm = normalize(v_Normalize);
+    vec3 lightDir = normalize(u_lightPos - v_FragPos);
+    float diff = max(dot(norm, lightDir), 0.0);
+    vec3 diffuse = diff * u_lightColor;
+
+    vec3 result = (ambient + diffuse) * u_objectColor;
+    gl_FragColor = vec4(result, 1.0);
 }
