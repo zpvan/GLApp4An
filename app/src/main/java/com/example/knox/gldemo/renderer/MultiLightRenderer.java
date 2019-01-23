c3d734a12b55360142f33a737df02d64d9337b09
zpvan <843762968@qq.com>
2019-01-23 10:07:16 +0800

    多光源场景, 但是聚光灯好像效果不明显

--- app/src/main/java/com/example/knox/gldemo/renderer/MultiLightRenderer.java
+++ app/src/main/java/com/example/knox/gldemo/renderer/MultiLightRenderer.java
@@ -33,7 +33,6 @@ public class MultiLightRenderer implements GLSurfaceView.Renderer {
     private static final String U_LIGHTCOLOR = "u_lightColor";
     private static final String A_NORMALIZE = "a_Normalize";
     private static final String A_TEXTURECOORDS = "a_TextureCoords";
-    private static final String U_LIGHTPOS = "u_lightPos";
     private static final String U_NORMALIZEMODEL = "u_NormalizeModel";
     private static final String U_VIEWPOS = "u_ViewPos";
     private static final String U_MATERIAL_AMBIENT = "u_Material.ambient";
@@ -92,6 +91,9 @@ public class MultiLightRenderer implements GLSurfaceView.Renderer {
     private static final String U_SPOTLIGHT_POSITION = "u_SpotLight.position";
     private static final String U_SPOTLIGHT_CUTOFF = "u_SpotLight.cutOff";
     private static final String U_SPOTLIGHT_OUTERCUTOFF = "u_SpotLight.outerCutOff";
+    private static final String U_SPOTLIGHT_CONSTANT = "u_SpotLight.constant";
+    private static final String U_SPOTLIGHT_LINEAR = "u_SpotLight.linear";
+    private static final String U_SPOTLIGHT_QUADRATIC = "u_SpotLight.quadratic";
     private static final String U_SPOTLIGHT_AMBIENT = "u_SpotLight.ambient";
     private static final String U_SPOTLIGHT_DIFFUSE = "u_SpotLight.diffuse";
     private static final String U_SPOTLIGHT_SPECULAR = "u_SpotLight.specular";
@@ -100,7 +102,7 @@ public class MultiLightRenderer implements GLSurfaceView.Renderer {
     private static final int NORMALIZE_COMPONENT_COUNT = 3;
     private static final int TEXTURE_COMPONENT_COUNT = 2;
 
-    private final float[] mEyePosition = {0.0f, 1.7f, 5.1f};
+    private final float[] mEyePosition = {0.0f, 1.7f, 10.1f};
     private final float[] mEyeFront = {0.0f, 0.0f, -1.0f};
 
     private Context mCtx;
@@ -115,7 +117,6 @@ public class MultiLightRenderer implements GLSurfaceView.Renderer {
     private int uObjectLightColor;
     private int uLightMatrix;
     private int aLightPosition;
-    private int uLightPos;
     private int uNormalizeModel;
     private int uViewPos;
     private int uMaterialAmbient;
@@ -125,6 +126,71 @@ public class MultiLightRenderer implements GLSurfaceView.Renderer {
     private int mWoodenDiffuseTex;
     private int mWoodenSpecularTex;
     private int mTextureCoords;
+
+    // 方向光
+    private int uDirLightDirection;
+    private int uDirLightAmbient;
+    private int uDirLightDiffuse;
+    private int uDirLightSpecular;
+
+    // 点光源
+    private int uPointLights0Position;
+    private int uPointLights0Constant;
+    private int uPointLights0Linear;
+    private int uPointLights0Quadratic;
+    private int uPointLights0Ambient;
+    private int uPointLights0Diffuse;
+    private int uPointLights0Specular;
+
+    private int uPointLights1Position;
+    private int uPointLights1Constant;
+    private int uPointLights1Linear;
+    private int uPointLights1Quadratic;
+    private int uPointLights1Ambient;
+    private int uPointLights1Diffuse;
+    private int uPointLights1Specular;
+
+    private int uPointLights2Position;
+    private int uPointLights2Constant;
+    private int uPointLights2Linear;
+    private int uPointLights2Quadratic;
+    private int uPointLights2Ambient;
+    private int uPointLights2Diffuse;
+    private int uPointLights2Specular;
+
+    private int uPointLights3Position;
+    private int uPointLights3Constant;
+    private int uPointLights3Linear;
+    private int uPointLights3Quadratic;
+    private int uPointLights3Ambient;
+    private int uPointLights3Diffuse;
+    private int uPointLights3Specular;
+
+    // 聚光灯
+    private int uSpotLightDirection;
+    private int uSpotLightPosition;
+    private int uSpotLightConstant;
+    private int uSpotLightLinear;
+    private int uSpotLightQuadratic;
+    private int uSpotLightCutOff;
+    private int uSpotLightOutCutOff;
+    private int uSpotLightAmbient;
+    private int uSpotLightDiffuse;
+    private int uSpotLightSpecular;
+
+    // 10个立方体的位置
+    private final float[][] cubePositions = new float[][] {
+            {0.0f,  0.0f,  0.0f},
+            {2.0f,  5.0f, -15.0f},
+            {-1.5f, -2.2f, -2.5f},
+            {-3.8f, -2.0f, -12.3f},
+            {2.4f, -0.4f, -3.5f},
+            {-1.7f,  3.0f, -7.5f},
+            {1.3f, -2.0f, -2.5f},
+            {1.5f,  2.0f, -2.5f},
+            {1.5f,  0.2f, -1.5f},
+            {-1.3f,  1.0f, -1.5f},
+    };
 
     float boxVertices[] = {
             //  X      Y      Z
@@ -304,7 +370,6 @@ public class MultiLightRenderer implements GLSurfaceView.Renderer {
         uObjectObjecColor = GLES20.glGetUniformLocation(mObjectProgram, U_OBJECTCOLOR);
         uObjectLightColor = GLES20.glGetUniformLocation(mObjectProgram, U_LIGHTCOLOR);
         aNormalize = GLES20.glGetAttribLocation(mObjectProgram, A_NORMALIZE);
-        uLightPos = GLES20.glGetAttribLocation(mObjectProgram, U_LIGHTPOS);
         uNormalizeModel = GLES20.glGetAttribLocation(mObjectProgram, U_NORMALIZEMODEL);
         uViewPos = GLES20.glGetAttribLocation(mObjectProgram, U_VIEWPOS);
         uMaterialAmbient = GLES20.glGetUniformLocation(mObjectProgram, U_MATERIAL_AMBIENT);
@@ -312,6 +377,57 @@ public class MultiLightRenderer implements GLSurfaceView.Renderer {
         uMaterialSpecular = GLES20.glGetUniformLocation(mObjectProgram, U_MATERIAL_SPECULAR);
         uMaterialShininess = GLES20.glGetUniformLocation(mObjectProgram, U_MATERIAL_SHININESS);
         mTextureCoords = GLES20.glGetAttribLocation(mObjectProgram, A_TEXTURECOORDS);
+
+        // 方向光
+        uDirLightDirection = GLES20.glGetUniformLocation(mObjectProgram, U_DIRLIGHT_DIRECTION);
+        uDirLightAmbient = GLES20.glGetUniformLocation(mObjectProgram, U_DIRLIGHT_AMBIENT);
+        uDirLightDiffuse = GLES20.glGetUniformLocation(mObjectProgram, U_DIRLIGHT_DIFFUSE);
+        uDirLightSpecular = GLES20.glGetUniformLocation(mObjectProgram, U_DIRLIGHT_SPECULAR);
+
+        // 点光源
+        uPointLights0Position = GLES20.glGetUniformLocation(mObjectProgram, U_POINTLIGHTS_0_POSITION);
+        uPointLights0Constant = GLES20.glGetUniformLocation(mObjectProgram, U_POINTLIGHTS_0_CONSTANT);
+        uPointLights0Linear = GLES20.glGetUniformLocation(mObjectProgram, U_POINTLIGHTS_0_LINEAR);
+        uPointLights0Quadratic = GLES20.glGetUniformLocation(mObjectProgram, U_POINTLIGHTS_0_QUADRATIC);
+        uPointLights0Ambient = GLES20.glGetUniformLocation(mObjectProgram, U_POINTLIGHTS_0_AMBIENT);
+        uPointLights0Diffuse = GLES20.glGetUniformLocation(mObjectProgram, U_POINTLIGHTS_0_DIFFUSE);
+        uPointLights0Specular = GLES20.glGetUniformLocation(mObjectProgram, U_POINTLIGHTS_0_SPECULAR);
+
+        uPointLights1Position = GLES20.glGetUniformLocation(mObjectProgram, U_POINTLIGHTS_1_POSITION);
+        uPointLights1Constant = GLES20.glGetUniformLocation(mObjectProgram, U_POINTLIGHTS_1_CONSTANT);
+        uPointLights1Linear = GLES20.glGetUniformLocation(mObjectProgram, U_POINTLIGHTS_1_LINEAR);
+        uPointLights1Quadratic = GLES20.glGetUniformLocation(mObjectProgram, U_POINTLIGHTS_1_QUADRATIC);
+        uPointLights1Ambient = GLES20.glGetUniformLocation(mObjectProgram, U_POINTLIGHTS_1_AMBIENT);
+        uPointLights1Diffuse = GLES20.glGetUniformLocation(mObjectProgram, U_POINTLIGHTS_1_DIFFUSE);
+        uPointLights1Specular = GLES20.glGetUniformLocation(mObjectProgram, U_POINTLIGHTS_1_SPECULAR);
+
+        uPointLights2Position = GLES20.glGetUniformLocation(mObjectProgram, U_POINTLIGHTS_2_POSITION);
+        uPointLights2Constant = GLES20.glGetUniformLocation(mObjectProgram, U_POINTLIGHTS_2_CONSTANT);
+        uPointLights2Linear = GLES20.glGetUniformLocation(mObjectProgram, U_POINTLIGHTS_2_LINEAR);
+        uPointLights2Quadratic = GLES20.glGetUniformLocation(mObjectProgram, U_POINTLIGHTS_2_QUADRATIC);
+        uPointLights2Ambient = GLES20.glGetUniformLocation(mObjectProgram, U_POINTLIGHTS_2_AMBIENT);
+        uPointLights2Diffuse = GLES20.glGetUniformLocation(mObjectProgram, U_POINTLIGHTS_2_DIFFUSE);
+        uPointLights2Specular = GLES20.glGetUniformLocation(mObjectProgram, U_POINTLIGHTS_2_SPECULAR);
+
+        uPointLights3Position = GLES20.glGetUniformLocation(mObjectProgram, U_POINTLIGHTS_3_POSITION);
+        uPointLights3Constant = GLES20.glGetUniformLocation(mObjectProgram, U_POINTLIGHTS_3_CONSTANT);
+        uPointLights3Linear = GLES20.glGetUniformLocation(mObjectProgram, U_POINTLIGHTS_3_LINEAR);
+        uPointLights3Quadratic = GLES20.glGetUniformLocation(mObjectProgram, U_POINTLIGHTS_3_QUADRATIC);
+        uPointLights3Ambient = GLES20.glGetUniformLocation(mObjectProgram, U_POINTLIGHTS_3_AMBIENT);
+        uPointLights3Diffuse = GLES20.glGetUniformLocation(mObjectProgram, U_POINTLIGHTS_3_DIFFUSE);
+        uPointLights3Specular = GLES20.glGetUniformLocation(mObjectProgram, U_POINTLIGHTS_3_SPECULAR);
+
+        // 聚光灯
+        uSpotLightDirection = GLES20.glGetUniformLocation(mObjectProgram, U_SPOTLIGHT_DIRECTION);
+        uSpotLightPosition = GLES20.glGetUniformLocation(mObjectProgram, U_SPOTLIGHT_POSITION);
+        uSpotLightCutOff = GLES20.glGetUniformLocation(mObjectProgram, U_SPOTLIGHT_CUTOFF);
+        uSpotLightOutCutOff = GLES20.glGetUniformLocation(mObjectProgram, U_SPOTLIGHT_OUTERCUTOFF);
+        uSpotLightAmbient = GLES20.glGetUniformLocation(mObjectProgram, U_SPOTLIGHT_AMBIENT);
+        uSpotLightDiffuse = GLES20.glGetUniformLocation(mObjectProgram, U_SPOTLIGHT_DIFFUSE);
+        uSpotLightSpecular = GLES20.glGetUniformLocation(mObjectProgram, U_SPOTLIGHT_SPECULAR);
+        uSpotLightConstant = GLES20.glGetUniformLocation(mObjectProgram, U_SPOTLIGHT_CONSTANT);
+        uSpotLightLinear = GLES20.glGetUniformLocation(mObjectProgram, U_SPOTLIGHT_LINEAR);
+        uSpotLightQuadratic = GLES20.glGetUniformLocation(mObjectProgram, U_SPOTLIGHT_QUADRATIC);
 
         mWoodenDiffuseTex = EsUtil.createTexture2D(mCtx, R.drawable.wooden_box_diff_tex);
         mWoodenSpecularTex = EsUtil.createTexture2D(mCtx, R.drawable.wooden_box_spe_tex);
@@ -330,9 +446,9 @@ public class MultiLightRenderer implements GLSurfaceView.Renderer {
         for (int i = 0; i < 10; i++) {
             float[] modelMatrix = new float[16];
             Matrix.setIdentityM(modelMatrix, 0);
-            Matrix.translateM(modelMatrix, 0, (float) (Math.random() * (i / 2 == 0 ? 1 : (-1)) * (20.0f)), 0, (float) (-30.0f * Math.random() - 2.0f));
-            Matrix.rotateM(modelMatrix, 0, (float) (180.0f * Math.random()), 0.5f, 1f, 0f);
-            // GLES20.glUniformMatrix4fv(uObjectModel, 1, false, mModelMatrix, 0);
+            Matrix.translateM(modelMatrix, 0, cubePositions[i][0], cubePositions[i][1], cubePositions[i][2]);
+            float angle = 20.0f * i;
+            Matrix.rotateM(modelMatrix, 0, angle, 1.0f, 0.3f, 0.5f);
             mModelMatrixs[i] = modelMatrix;
 
             float[] transposeInvertModelMatrix = new float[16];
@@ -386,8 +502,8 @@ public class MultiLightRenderer implements GLSurfaceView.Renderer {
     public void onDrawFrame(GL10 gl) {
         GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT | GLES20.GL_DEPTH_BUFFER_BIT);
 
-        GLES20.glUseProgram(mObjectProgram);
         // 画立方体
+        GLES20.glUseProgram(mObjectProgram);
         EsUtil.VertexAttribArrayAndEnable(aObjectPosition, POSITION_COMPONENT_COUNT, GLES20.GL_FLOAT,
                 false, 0, boxVertices);
 
@@ -399,15 +515,60 @@ public class MultiLightRenderer implements GLSurfaceView.Renderer {
 
         GLES20.glUniform3f(uObjectObjecColor, 1.0f, 0.5f, 0.31f);
         GLES20.glUniform3f(uObjectLightColor, 1.0f, 1.0f, 1.0f);
-        GLES20.glUniform3f(uLightPos, mLightPosition[0], mLightPosition[1], mLightPosition[2]);
         GLES20.glUniform3f(uViewPos, mEyePosition[0], mEyePosition[1], mEyePosition[2]);
 
         GLES20.glUniform1i(uMaterialDiffuse, 0);
         GLES20.glUniform1i(uMaterialSpecular, 1);
         GLES20.glUniform1f(uMaterialShininess, 128 * 0.6f);
 
-
-
+        // 方向光
+        GLES20.glUniform3f(uDirLightDirection, -0.2f, -1.0f, -0.3f);
+        GLES20.glUniform3f(uDirLightAmbient, 0.05f, 0.05f, 0.05f);
+        GLES20.glUniform3f(uDirLightDiffuse, 0.4f, 0.4f, 0.4f);
+        GLES20.glUniform3f(uDirLightSpecular, 0.5f, 0.5f, 0.5f);
+        // 点光源1
+        GLES20.glUniform3f(uPointLights0Position, pointLightPositions[0][0], pointLightPositions[0][1], pointLightPositions[0][2]);
+        GLES20.glUniform3f(uPointLights0Ambient, 0.05f, 0.05f, 0.05f);
+        GLES20.glUniform3f(uPointLights0Diffuse, 0.8f, 0.8f, 0.8f);
+        GLES20.glUniform3f(uPointLights0Specular, 1.0f, 1.0f, 1.0f);
+        GLES20.glUniform1f(uPointLights0Constant, 1.0f);
+        GLES20.glUniform1f(uPointLights0Linear, 0.09f);
+        GLES20.glUniform1f(uPointLights0Quadratic, 0.032f);
+        // 点光源2
+        GLES20.glUniform3f(uPointLights1Position, pointLightPositions[1][0], pointLightPositions[1][1], pointLightPositions[1][2]);
+        GLES20.glUniform3f(uPointLights1Ambient, 0.05f, 0.05f, 0.05f);
+        GLES20.glUniform3f(uPointLights1Diffuse, 0.8f, 0.8f, 0.8f);
+        GLES20.glUniform3f(uPointLights1Specular, 1.0f, 1.0f, 1.0f);
+        GLES20.glUniform1f(uPointLights1Constant, 1.0f);
+        GLES20.glUniform1f(uPointLights1Linear, 0.09f);
+        GLES20.glUniform1f(uPointLights1Quadratic, 0.032f);
+        // 点光源3
+        GLES20.glUniform3f(uPointLights2Position, pointLightPositions[2][0], pointLightPositions[2][1], pointLightPositions[2][2]);
+        GLES20.glUniform3f(uPointLights2Ambient, 0.05f, 0.05f, 0.05f);
+        GLES20.glUniform3f(uPointLights2Diffuse, 0.8f, 0.8f, 0.8f);
+        GLES20.glUniform3f(uPointLights2Specular, 1.0f, 1.0f, 1.0f);
+        GLES20.glUniform1f(uPointLights2Constant, 1.0f);
+        GLES20.glUniform1f(uPointLights2Linear, 0.09f);
+        GLES20.glUniform1f(uPointLights2Quadratic, 0.032f);
+        // 点光源4
+        GLES20.glUniform3f(uPointLights3Position, pointLightPositions[3][0], pointLightPositions[3][1], pointLightPositions[3][2]);
+        GLES20.glUniform3f(uPointLights3Ambient, 0.05f, 0.05f, 0.05f);
+        GLES20.glUniform3f(uPointLights3Diffuse, 0.8f, 0.8f, 0.8f);
+        GLES20.glUniform3f(uPointLights3Specular, 1.0f, 1.0f, 1.0f);
+        GLES20.glUniform1f(uPointLights3Constant, 1.0f);
+        GLES20.glUniform1f(uPointLights3Linear, 0.09f);
+        GLES20.glUniform1f(uPointLights3Quadratic, 0.032f);
+        // 聚光灯
+        GLES20.glUniform3f(uSpotLightPosition, mEyePosition[0], mEyePosition[1], mEyePosition[2]);
+        GLES20.glUniform3f(uSpotLightDirection, mEyeFront[0], mEyeFront[1], mEyeFront[2]);
+        GLES20.glUniform3f(uSpotLightAmbient, 0.0f, 0.0f, 0.0f);
+        GLES20.glUniform3f(uSpotLightDiffuse, 1.0f, 1.0f, 1.0f);
+        GLES20.glUniform3f(uSpotLightSpecular, 1.0f, 1.0f, 1.0f);
+        GLES20.glUniform1f(uSpotLightConstant, 1.0f);
+        GLES20.glUniform1f(uSpotLightLinear, 0.09f);
+        GLES20.glUniform1f(uSpotLightQuadratic, 0.032f);
+        GLES20.glUniform1f(uSpotLightCutOff, (float) Math.cos(12.5f));
+        GLES20.glUniform1f(uSpotLightOutCutOff, (float) Math.cos(15.0f));
         for (int i = 0; i < 10; i++) {
             GLES20.glUniformMatrix4fv(uObjectModel, 1, false, mModelMatrixs[i], 0);
             GLES20.glUniformMatrix4fv(uNormalizeModel, 1, false, mTransposeInvertModelMatrixs[i], 0);
@@ -416,17 +577,20 @@ public class MultiLightRenderer implements GLSurfaceView.Renderer {
         }
         GLES20.glUseProgram(0);
 
-        GLES20.glUseProgram(mLightProgram);
         // 画光源
+        GLES20.glUseProgram(mLightProgram);
         EsUtil.VertexAttribArrayAndEnable(aLightPosition, POSITION_COMPONENT_COUNT, GLES20.GL_FLOAT,
                 false, 0, boxVertices);
-        Matrix.setIdentityM(mModelMatrix, 0);
-        Matrix.translateM(mModelMatrix, 0, mLightPosition[0], mLightPosition[1], mLightPosition[2]);
-        Matrix.scaleM(mModelMatrix, 0, 0.1f, 0.1f, 0.1f);
-        Matrix.multiplyMM(mMVPMatrix, 0, mViewProjectionMatrix, 0, mModelMatrix, 0);
-        GLES20.glUniformMatrix4fv(uLightMatrix, 1, false, mMVPMatrix, 0);
 
-        GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, boxVertices.length / POSITION_COMPONENT_COUNT);
+        for (int i = 0; i < 4; i++) {
+            final float[] modelMatrix = new float[16];
+            Matrix.setIdentityM(modelMatrix, 0);
+            Matrix.translateM(modelMatrix, 0, pointLightPositions[i][0], pointLightPositions[i][1], pointLightPositions[i][2]);
+            Matrix.scaleM(modelMatrix, 0, 0.1f, 0.1f, 0.1f);
+            Matrix.multiplyMM(mMVPMatrix, 0, mViewProjectionMatrix, 0, modelMatrix, 0);
+            GLES20.glUniformMatrix4fv(uLightMatrix, 1, false, mMVPMatrix, 0);
+            GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, boxVertices.length / POSITION_COMPONENT_COUNT);
+        }
         GLES20.glUseProgram(0);
     }
 }
--- app/src/main/res/raw/multi_light_fragment_shader.glsl
+++ app/src/main/res/raw/multi_light_fragment_shader.glsl
@@ -3,7 +3,6 @@ precision mediump float;
 uniform vec3 u_objectColor;
 uniform vec3 u_lightColor;
 
-uniform vec3 u_lightPos;
 uniform vec3 u_ViewPos;
 
 varying vec3 v_Normalize;
@@ -29,7 +28,6 @@ struct DirLight {
 
 uniform DirLight u_DirLight;
 
-// 计算方向光的影响
 vec3 CalcDirLight(DirLight light, vec3 normal, vec3 viewDir) {
     vec3 lightDir = normalize(-light.direction);
 
@@ -63,19 +61,18 @@ struct PointLight {
 #define NR_POINT_LIGHTS 4
 uniform PointLight u_PointLights[NR_POINT_LIGHTS];
 
-// 计算点光源的影响
 vec3 CalcPointLight(PointLight light, vec3 normal, vec3 fragPos, vec3 viewDir) {
-    // 环境光
     vec3 ambient = light.ambient * vec3(texture2D(u_Material.diffuse, v_TextureCoords));
 
-    // 漫反射光
     vec3 norm = normalize(normal);
     vec3 lightDir = normalize(light.position - v_FragPos);
-
     float diff = max(dot(norm, lightDir), 0.0);
     vec3 diffuse = light.diffuse * diff * vec3(texture2D(u_Material.specular, v_TextureCoords));
 
-    // 衰减
+    vec3 reflectDir = reflect(-lightDir, norm);
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), u_Material.shininess);
+    vec3 specular = light.specular * spec * vec3(texture2D(u_Material.specular, v_TextureCoords));
+
     float distance = length(light.position - v_FragPos);
     float attenuation = 1.0 / (light.constant + light.linear * distance + light.quadratic * (distance * distance));
     ambient *= attenuation;
@@ -90,8 +87,11 @@ struct SpotLight {
     vec3 direction;
     vec3 position;
     float cutOff;
-    // 外椎角
     float outerCutOff;
+
+    float constant;
+    float linear;
+    float quadratic;
 
     vec3 ambient;
     vec3 diffuse;
@@ -99,35 +99,25 @@ struct SpotLight {
 };
 uniform SpotLight u_SpotLight;
 
-// 计算聚光灯的影响
-vec3 CalcPointLight(SpotLight light, vec3 normal, vec3 fragPos, vec3 viewDir) {
-    // 环境光
+vec3 CalcSpotLight(SpotLight light, vec3 normal, vec3 fragPos, vec3 viewDir) {
     vec3 ambient = light.ambient * vec3(texture2D(u_Material.diffuse, v_TextureCoords));
 
-    // 漫反射光
     vec3 norm = normalize(normal);
     vec3 lightDir = normalize(light.position - fragPos);
-
     float diff = max(dot(norm, lightDir), 0.0);
     vec3 diffuse = light.diffuse * diff * vec3(texture2D(u_Material.diffuse, v_TextureCoords));
 
-    // 镜面高光
     vec3 reflectDir = reflect(-lightDir, norm);
     float spec = pow(max(dot(viewDir, reflectDir), 0.0), u_Material.shininess);
     vec3 specular = light.specular * spec * vec3(texture2D(u_Material.specular, v_TextureCoords));
 
-    // 聚光灯
-    // 计算片元角度的cos值
     float theta = dot(lightDir, normalize(-light.direction));
-    // 计算epsilon的值, 用内椎角的cos值减去外椎角的cos值
     float epsilon = light.cutOff - light.outerCutOff;
-    // 根据公式计算光照强度, 并限制结果的范围
     float intensity = clamp((theta - light.outerCutOff) / epsilon, 0.0, 1.0);
 
     diffuse *= intensity;
     specular *= intensity;
 
-    // 衰减
     float distance = length(light.position - v_FragPos);
     float attenuation = 1.0 / (light.constant + light.linear * distance + light.quadratic * (distance * distance));
     ambient *= attenuation;
@@ -140,16 +130,9 @@ vec3 CalcPointLight(SpotLight light, vec3 normal, vec3 fragPos, vec3 viewDir) {
 void main() {
     vec3 norm = normalize(v_Normalize);
     vec3 viewDir = normalize(u_ViewPos - v_FragPos);
-
-    // 方向光
     vec3 result = CalcDirLight(u_DirLight, norm, viewDir);
-
-    // 点光源
     for (int i = 0; i < NR_POINT_LIGHTS; ++i)
         result += CalcPointLight(u_PointLights[i], norm, v_FragPos, viewDir);
-
-    // 聚光灯
     result += CalcSpotLight(u_SpotLight, norm, v_FragPos, viewDir);
-
     gl_FragColor = vec4(result, 1.0);
 }
